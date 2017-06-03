3d787099179642ff22282033793edd7f34a2a042
Megan Coyle <meg@mcoyle.com>
2017-06-03 12:47:55 -0400

    Populating tooltip container

--- includes/tooltip.js
+++ includes/tooltip.js
@@ -4,6 +4,12 @@ $(document).ready(function(){
     if($(this).attr('data-tip-type') == 'text') {
       $('#tooltip-container').html($(this).attr('data-tip-source'));
     }
+
+    if($(this).attr('data-tip-type') == 'html') {
+      var elementToGet = '#' + $(this).attr('data-tip-source');
+      var newHTML = $(elementToGet).html();
+      $('#tooltip-container').html(newHTML);
+    }
   }).mousemove(function(e){
 
   }).mouseout(function(e){
